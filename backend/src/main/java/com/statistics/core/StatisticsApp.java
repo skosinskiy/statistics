5059fea3b297c74723332f040dada457230f8682
Stanislav Kosinskyi <stanislav.kosinski@gmail.com>
2020-09-17 17:53:35 +0300

    Added codecov

--- backend/src/main/java/com/statistics/core/StatisticsApp.java
+++ backend/src/main/java/com/statistics/core/StatisticsApp.java
@@ -12,6 +12,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class StatisticsApp {
 
   public static void main(String[] args) {
+    int b = 0;
+    for (int i = 0; i < 100000; i++) {
+      b = i;
+    }
     SpringApplication.run(StatisticsApp.class);
   }
 

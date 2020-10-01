e46871eea36d7af32184e6cf8a3065b1bf4042c6
Алексей Дроздюк <a.drozdyuk@lanet.ua>
2020-10-01 18:23:06 +0300

    to do style round item, and logic render in one line of column

---
+++ frontend/src/pages/MainPage/Sports/RoundItem/RoundItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const RoundItem = (props) => {
+  console.log(props)
+  const { date, homeTeam, awayTeam, homeScore, state } = props
+
+  return (<div className="item-wrapper">match</div>
+  )
+}
--- frontend/src/pages/MainPage/Sports/Sports.js
+++ frontend/src/pages/MainPage/Sports/Sports.js
@@ -2,14 +2,23 @@ import React, { useEffect, useState } from 'react'
 import { Redirect } from 'react-router-dom'
 // components
 import { LinearIndeterminate } from '../../../components/LinearIndeterminate/LinearIndeterminate'
+import Accordion from '@material-ui/core/Accordion'
+import AccordionSummary from '@material-ui/core/AccordionSummary'
+import AccordionDetails from '@material-ui/core/AccordionDetails'
+import Typography from '@material-ui/core/Typography'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+// child
+import { RoundItem } from './RoundItem/RoundItem'
 // redux
 import { useSelector, useDispatch } from 'react-redux'
 import { setChoiceCategoryOfSports } from '../../../store/sportsCategory/operations'
 import { findAllRounds } from '../../../store/rounds/operations'
+import './Sports.scss'
 
 export const Sports = (props) => {
   const dispatch = useDispatch()
   const [redirctToHome, setRedirctToHome] = useState(false)
+  const [expanded, setExpanded] = useState('panel1')
 
   const sports = (useSelector(state => state.sportsCategory.sportsCategory))
 
@@ -33,7 +42,9 @@ export const Sports = (props) => {
     }
     if (activeIdOfTournament && !isTournamentsLoading) dispatch(findAllRounds(activeIdOfTournament))
   }, [isTournamentsLoading, tournaments, sports, props.match.params.id, activeIdOfTournament, dispatch])
-
+  const penalChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false)
+  }
   if (roundsOfChoiceTournament != null && !isRoundsLoading && !isTournamentsLoading) {
     console.log(roundsOfChoiceTournament.content)
   }
@@ -43,8 +54,28 @@ export const Sports = (props) => {
 
   return (
     <React.Fragment>
-      { isTournamentsLoading ? <LinearIndeterminate></LinearIndeterminate> : <div>
-        We in sports component info;
+      { isTournamentsLoading ? <LinearIndeterminate></LinearIndeterminate> : <div className="rounds">
+        { roundsOfChoiceTournament ? roundsOfChoiceTournament.content.map((round) => {
+          return <Accordion key={'key_' + round.id}
+            square expanded={expanded === 'panel_id_' + round.id}
+            onChange={penalChange('panel_id_' + round.id)}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography className="accordion-title">{'Round' + round.number}</Typography>
+            </AccordionSummary>
+            <AccordionDetails className="accordion-content">
+              {
+                round.matchList.map((item) => {
+                  return <RoundItem key={'key_' + item.id} item={item}></RoundItem>
+                })
+              }
+            </AccordionDetails>
+          </Accordion>
+        }) : null
+        }
       </div> }
     </React.Fragment>
   )
